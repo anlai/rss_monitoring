fb8964821d65d59bedcc682c1e278cd8e1351aa5
Alan Lai <alan.n.lai@gmail.com>
2022-05-15 08:51:25 -0700

    dockerized + email

--- rpilocator.py
+++ rpilocator.py
@@ -1,27 +1,18 @@
 # Load pkgs
-from config.secrets import *
 from twilio.rest import Client
 import feedparser
-import hashlib
-import json
 import time
 import pytz
 from datetime import date, datetime
+import smtplib, ssl
+
+import settings
 
 # Setup Twilio Client for sending messages
-client = Client(TWILIO_SID, TWILIO_TOKEN)
+client = Client(settings.TWILIO_SID, settings.TWILIO_TOKEN)
 
 # Set Timezone to UTC/GMT
 UTC = pytz.utc
-
-# Config object
-
-config = {
-    "URL": "https://rpilocator.com/feed/?country=US",
-    "waitTime": 15, # in seconds
-}
-
-# Target URL.
 
 # Function to send notification (Current: Message by Twilio)
 def sendNoti(feed):
@@ -40,41 +31,79 @@ def sendNoti(feed):
             msg = msg + title + "\n" + link + "\n" + published + "\n------------\n"
 
     # Send the built message
+    match settings.NOTIFICATION:
+        case 'twilio':
+            sendTwilio(msg)
+        case 'email':
+            sendEmail(msg)
+
+    debugOutput(f'Sent msg: {msg}')
+
+def sendTwilio(msg):
     message = client.messages \
                 .create(
                      body=msg,
-                     from_=TWILIO_FROM,
-                     to=TWILIO_TO
+                     from_=settings.TWILIO_FROM,
+                     to=settings.TWILIO_TO
                  )
-    # For debugging purpose.
 
-    print(f'Sent msg: {msg}')
+def sendEmail(msg):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(settings.EMAIL_SERVER, settings.EMAIL_PORT, context=context) as server:
+        server.login(settings.EMAIL_USER, settings.EMAIL_PASS)
+        server.sendmail(settings.EMAIL_USER, settings.EMAIL_TO, msg)
+
+# calls to load data from the feed, including retry logic
+def loadFeed():
+
+    debugOutput("Attempting to load feed data")
+
+    feed = feedparser.parse(settings.RSS_URL)
+
+    debugOutput(f"Received {feed.status} from call")
+
+    while feed.status != 200:
+        if feed.status == 429:
+            retryAfter = int(feed.headers['retry-after'])
+            debugOutput(f"Waiting for {retryAfter} seconds for retry...")
+            time.sleep(retryAfter+1) # wait an additional second
+        else:
+            debugOutput(f"Recieved status {feed.status}, waiting normal interval for retry...")
+            time.sleep(settings.INTERVAL)
+        
+        feed = feedparser.parse(settings.RSS_URL)
+
+    return feed
+
+def debugOutput(msg):
+    if settings.VERBOSE:
+        print(f"DEBUG: {msg}")
 
 # Driver Function
 def main():
+
+    print("Initializing...")
+
     # Get the data
-    feed = feedparser.parse(config["URL"])
+    feed = loadFeed()
 
     # Get latest changes made
     default = feed.entries[0].published
 
-    # Default hashed value
-    # default = get_hash(feed.entries).hex()
-    
     print("Start!")
-    print(f"Target URL: {config['URL']}")
+    print(f"Target URL: {settings.RSS_URL}")
     print("." + "\n" + "." + "\n" + "." + "\n")
 
     while True:
         try:
             print(f"Monitoring the RSS...")
-            time.sleep(config["waitTime"])
+            time.sleep(settings.INTERVAL)
             # Update current time
             currentTime = datetime.now(UTC)
             currentTime = currentTime.strftime("%H:%M:%S")
 
             # Get new data
-            feed = feedparser.parse(config['URL'])
+            feed = loadFeed()
 
             # Get the lastest published time again to check for any changes
             check = feed.entries[0].published
@@ -85,9 +114,8 @@ def main():
                 sendNoti(feed)
                 print(f"--->Changes detected! Message sent at {currentTime}<---")
                 print("--------------------------\n")
-                print(f'For debugging: ')
-                print(f'Default (before changed): {default}')
-                print(f'Check: {check}')
+                debugOutput(f'Default (before changed): {default}')
+                debugOutput(f'Check: {check}')
 
                 # Update default with new changes
                 default = check
@@ -103,7 +131,5 @@ def main():
             print("Exception message: {}".format(e))
             print("-----------------------------")
             
-
-
 if __name__ == "__main__":
     main()
